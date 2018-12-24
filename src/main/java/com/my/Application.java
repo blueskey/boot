c11336198db39028fff8d947e9a41690a000ffdb
blueskey <jingyu3hao@gmail.com>
2018-12-24 13:56:17 +0800

    first commit

---
+++ src/main/java/com/my/Application.java
@@ -0,0 +1,21 @@
+package com.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@ResponseBody
+	@RequestMapping("/")
+	public String hello() {
+		return "hello";
+	}
+}
