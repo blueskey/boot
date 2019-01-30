186ef95380dfdb09c07b772a574758c53bb3d3c8
blueskey <jingyu3hao@gmail.com>
2019-01-30 14:40:39 +0800

    redis哨兵

--- src/test/java/HelloControllerTest.java
+++ src/test/java/HelloControllerTest.java
@@ -2,7 +2,10 @@ import com.my.Application;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -11,11 +14,19 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import javax.annotation.PostConstruct;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes =Application.class )
 public class HelloControllerTest {
 
 	private MockMvc mockMvc;
+
+
+	@Autowired StringRedisTemplate stringRedisTemplate;
+
+
+	ValueOperations<String, String> stringRedis;
 
 	@Before
 	public void setUp() throws Exception {
@@ -27,5 +38,19 @@ public class HelloControllerTest {
 				.andExpect(MockMvcResultMatchers.status().isOk())
 				.andDo(MockMvcResultHandlers.print())
 				.andReturn());;
+	}
+
+
+
+	@PostConstruct
+	public void init(){
+		stringRedis=stringRedisTemplate.opsForValue();
+	}
+
+
+	@Test
+	public void testString (){
+		stringRedis.set("sen", "sen1");
+		System.out.println(stringRedis.get("sen"));
 	}
 }
